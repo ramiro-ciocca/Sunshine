dd6daa05be88cc77e704da764447da8df0e67e0e
Ramiro Ciocca <ciocca@gmail.com>
2016-05-17 01:20:48 -0300

    Crea Adapter para poblar el ListView con los list items

--- app/src/main/java/com/lulisoft/sunshine/MainActivityFragment.java
+++ app/src/main/java/com/lulisoft/sunshine/MainActivityFragment.java
@@ -1,15 +1,23 @@
 package com.lulisoft.sunshine;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    ArrayAdapter<String> mForecastAdapter;
 
     public MainActivityFragment() {
     }
@@ -17,6 +25,22 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        String[] forecastArray = {
+                "Hoy - Soleado - 32º/22º",
+                "Mañana - Nublado - 22º/12º",
+                "Miércoles - Lluvioso - 12º/2º",
+                "Jueves - Nevando - 2º/-12º",
+                "Viernes - Soleado - 40º/28º",
+                "Sábado - Niebla - 22º/12º",
+                "Domingo - Granizo - 2º/-16º"
+        };
+        List<String> weekForecast = new ArrayList<>(Arrays.asList(forecastArray));
+        mForecastAdapter = new ArrayAdapter<>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, weekForecast);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        ListView listViewForecast = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listViewForecast.setAdapter(mForecastAdapter);
+
+        return rootView;
     }
 }
