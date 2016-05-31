d0dbc809fa29b9457e96fdd840285cd56d945403
Ramiro Ciocca <ciocca@gmail.com>
2016-05-31 01:15:50 -0300

    Agrega volley para conectarse a internet

--- app/src/main/java/com/lulisoft/sunshine/ForecastFragment.java
+++ app/src/main/java/com/lulisoft/sunshine/ForecastFragment.java
@@ -8,6 +8,13 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.android.volley.VolleyError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -15,11 +22,11 @@ import java.util.List;
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment {
+public class ForecastFragment extends Fragment {
 
     ArrayAdapter<String> mForecastAdapter;
 
-    public MainActivityFragment() {
+    public ForecastFragment() {
     }
 
     @Override
@@ -40,6 +47,26 @@ public class MainActivityFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
         ListView listViewForecast = (ListView) rootView.findViewById(R.id.listview_forecast);
         listViewForecast.setAdapter(mForecastAdapter);
+
+        String url ="http://api.openweathermap.org/data/2.5/forecast/daily?id=3441575&APPID=1de3e053cf901e47dd893b231706e656&mode=json&units=metrics&cnt=7";
+
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        System.out.println(response.toString());
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO Auto-generated method stub
+
+                    }
+                });
+
+        VolleySingleton.getInstance(getContext()).addToRequestQueue(jsObjRequest);
 
         return rootView;
     }
